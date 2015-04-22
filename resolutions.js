3ff24c555985d6a8a0106c3ee372edfc2207c0d1
tableforc <leonmakster@gmail.com>
2015-04-22 23:18:55 +0800

    added checkbox template helper and style for checked

--- resolutions.js
+++ resolutions.js
@@ -10,17 +10,26 @@ if (Meteor.isClient) {
   });
 
   Template.body.events({
-      'submit .new-resolution' :function(event){
-          var title = event.target.title.value;
+      'submit .new-resolution' :function(event){ /*key for this object is an event string: submit, look for .new-resolution, triggers event function below*/
+          var title = event.target.title.value;/*target event, get title (name of field), and value submitted*/
 
-          Resolutions.insert({
+          Resolutions.insert({/*save new key to db*/
             title/*of Resolutions*/: title, /*var title*/
             createdAt: new Date()
           });
 
-          event.target.title.value =""; /*clear form*/
+          event.target.title.value =""; /*clear input field in form*/
           return false;/*prevent refresh due to submit button*/
-      }
+       }
+  });
+
+  Template.resolution.events({ /*inside the resolution not body*/
+    'click .toggle-checked':function(){
+        Resolutions.update(this._id, {$set: {checked: !this.checked}})
+    }, /*need comma to separate 2 properties*/
+    'click .delete':function(){ /*listen for click event class delete*/
+        Resolutions.remove(this._id); /*in collection named Resolutions, remove the current key's id*/
+    }
   });
 }
 
